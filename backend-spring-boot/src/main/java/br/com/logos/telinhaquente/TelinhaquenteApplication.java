ca2b8b63259a488121a029cf0f77654b0375d555
Rafakka <rafaelkaher@gmail.com>
2025-07-25 08:42:47 -0300

    atualizando

--- backend-spring-boot/src/main/java/br/com/logos/telinhaquente/TelinhaquenteApplication.java
+++ backend-spring-boot/src/main/java/br/com/logos/telinhaquente/TelinhaquenteApplication.java
@@ -10,8 +10,7 @@ import java.util.Scanner;
 
 @SpringBootApplication
 @EnableConfigurationProperties(OmdbProperties.class)
-public class TelinhaquenteApplication {
-
+public class TelinhaquenteApplication { 
     public static void main(String[] args) {
         SpringApplication.run(TelinhaquenteApplication.class, args);
 
