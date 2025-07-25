6f24731976df82cb5109a804c7be9834a0430958
Rafakka <rafaelkaher@gmail.com>
2025-07-25 16:31:36 -0300

    packages bugando

--- backend-spring-boot/src/main/java/br/com/logos/telinhaquente/controller/MidiaController.java
+++ backend-spring-boot/src/main/java/br/com/logos/telinhaquente/controller/MidiaController.java
@@ -1,5 +1,6 @@
 package br.com.logos.telinhaquente.controller;
 
+import br.com.logos.telinhaquente.service.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
